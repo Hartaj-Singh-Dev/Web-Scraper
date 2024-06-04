e726bd7eb1e00ed2451417e2134a461d947a2a0d
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2024-06-04 21:03:13 +0530

    Will test it with my own client

--- server.js
+++ server.js
@@ -24,29 +24,6 @@ const PORT = process.env.SERVER_PORT_ID | 3000
 
 const url = 'https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers';
 
-// async function fetchCSVLink() {
-//   try {
-//     // Fetch the HTML content of the page
-//     const { data } = await axios.get(url);
-
-//     // Load the HTML into cheerio
-//     const $ = cheerio.load(data);
-
-//     // Find the link to the CSV file
-//     // const link = $('a:contains("CSV")').attr('href');
-//    const link =  $('a.govuk-link.gem-c-attachment__link').attr('href');
-//     if (link) {
-//       // Construct the full URL if the link is relative
-//       // const csvUrl = link.startsWith('http') ? link : `https://www.assests.publishing.service.gov.uk/{link}`;
-//       const csvUrl = link.startsWith('http') ? link : `https://www.assests.publishing.service.gov.uk${link}`;
-//       console.log('CSV Download Link:', csvUrl);
-//     } else {
-//       console.log('CSV link not found');
-//     }
-//   } catch (error) {
-//     console.error('Error fetching the CSV link:', error);
-//   }
-// }
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))

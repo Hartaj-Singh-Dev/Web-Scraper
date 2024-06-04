c3ee4a7092fa52f3335d651c402ced6f37cba738
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2024-06-04 11:41:44 +0530

    Hoping that it works on Production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 const app = express();
 import dotenv from "dotenv"
 import bodyParser from "body-parser";
+import cron from "node-cron"
 import { savetoDb } from "./service/savetoDB.js";
 import {getLink }from "./util/getLink.js"
 import fetchAndCov from "./util/fetchAndCov.js"
@@ -15,6 +16,7 @@ import connect from "./Database/connect.js"
 import extractDatefromURL from "./util/extractDatefromURL.js";
 
 connect(process.env.DATABASE)
+import comparisonJSONData from "./models/comparisonData.js";
 const PORT = process.env.SERVER_PORT_ID | 3000
 
 
@@ -52,23 +54,21 @@ app.use(cors({origin:process.env.CLIENT_SITE_LINK || "https://localhost:3000"}))
 
 
 // Here just running my CRON JOB on TIME
-// cron.schedule('30 21 * * 1-5', async () => {
-//   console.log('Cron job started at 9:30 PM, Monday to Friday');
+cron.schedule('30 21 * * 1-5', async () => {
+  console.log('Cron job started at 9:30 PM, Monday to Friday');
   
-//   try {
-//     // Step 1: Get the CSV link
-//     const csvUrl = await getLink();
+  try {
 
-//     // Step 2: Process the CSV data
-//     const jsonData = await fetchAndCov(csvUrl);
+     const csvURL=  await getLink(url)
+    const jsonDate = extractDatefromURL(csvURL)
+    const jsonData = await fetchAndCov(csvURL)
+    const difference = await savetoDb(jsonData , jsonDate)
+  } catch (error) {
+    console.error('Error in cron job:', error);
+  }
 
-//   const difference =   await saveToMongoDB(jsonData);
-//   } catch (error) {
-//     console.error('Error in cron job:', error);
-//   }
-
-//   console.log('Cron job finished');
-// });
+  console.log('Cron job finished');
+});
 
 
 
@@ -77,13 +77,8 @@ app.get("/" , (req , res)=>{
 })
 
 app.get("/getTodayCSV" ,async (req,res)=>{
-    const csvURL=  await getLink(url)
-    const jsonDate = extractDatefromURL(csvURL)
-    const jsonData = await fetchAndCov(csvURL)
-    const difference = await savetoDb(jsonData , jsonDate)
-    res.status(200).send(jsonData)
-    res.status(200).send(difference)
-    
+    const Difference = await comparisonJSONData.find()
+    res.status(200).json(Difference)
 })
 
 app.listen(PORT , ()=>{
